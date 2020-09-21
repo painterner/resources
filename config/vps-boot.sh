a3633a289a00d76b83a69a6d4f5500bfb6ab5cd0
painterner <i5stiyuki@icloud.com>
2020-09-21 15:48:52 +0800

    Update vps-boot.sh

--- config/vps-boot.sh
+++ config/vps-boot.sh
@@ -1,26 +1,26 @@
 # passwd need interactive
 # non-inactive mode > echo -e "passwd123\npasswd123" | passwd
 # ref: https://www.systutorials.com/changing-linux-users-password-in-one-command-line/
-echo 'please input you password'
+echo 'PTBOOT: please input you password'
 passwd
 
 apt update
 apt upgrade
 
-echo 'adding a new user'
+echo 'PTBOOT: adding a new user'
 adduser ka
-echo "adding ka to sudoers"
+echo "PTBOOT: adding ka to sudoers"
 echo "#ka     ALL=(ALL:ALL) ALL"            >> /etc/sudoers
 echo "#%ka    ALL=(ALL:ALL) ALL #add group" >> /etc/sudoers
 echo "ka      ALL=(ALL:ALL) NOPASSWD:ALL"   >> /etc/sudoers
 
-su ka
-cd ~
+sudo -u ka -H sh -c 'cd ~
 mkdir all cenv downloads music pictures videos
 cd all
 mkdir docs proj refs tools
 cd ~
 
-echo "adding npm"
+echo "PTBOOT: adding npm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-nvm install v12.16.1
+nvm instal v12.16.1
+'
