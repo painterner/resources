4d59b650913ad7c84223ee15ac1310658329b840
painterner <i5stiyuki@icloud.com>
2020-12-13 09:11:05 +0800

    Update vps-boot.sh

--- config/vps-boot.sh
+++ config/vps-boot.sh
@@ -7,7 +7,7 @@ passwd
 apt update
 apt upgrade
 
-apt install vim
+apt install vim -y
 
 echo 'PTBOOT: adding a new user'
 adduser ka
@@ -24,5 +24,28 @@ cd ~
 
 echo "PTBOOT: adding npm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-nvm instal v12.16.1
+#nvm install v12.16.1
 '
+## docker untested ?
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io #install docker engine
+apt-cache madison docker-ce # list docker installed
+
+#add user to docker group -> https://docs.docker.com/engine/install/linux-postinstall/
+su ka
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker # active
+docker run hello-world
