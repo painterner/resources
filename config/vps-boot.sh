4765d5bf2e117cc5afe97d7bafbb2020288c8314
painterner <i5stiyuki@icloud.com>
2020-12-13 09:34:19 +0800

    Update vps-boot.sh

--- config/vps-boot.sh
+++ config/vps-boot.sh
@@ -55,6 +55,13 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-##  kubernetes minikube tool (very small)
+##  kubernetes 
+## kubectl
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+## minikube tool (very small)
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
