6972084bb6ab02470ebdddb30e14670f23903c69
painterner <i5stiyuki@icloud.com>
2021-01-22 10:42:48 +0800

    Update vps-boot.sh

--- config/vps-boot.sh
+++ config/vps-boot.sh
@@ -65,3 +65,7 @@ sudo apt-get install -y kubectl
 ## minikube tool (very small)
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
+
+## fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
