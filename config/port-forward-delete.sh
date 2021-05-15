04d53a96c4cdc7d06cfb9b95797ae6299032e6b3
painterner <i5stiyuki@icloud.com>
2021-05-15 10:25:06 +0800

    Update port-forward-delete.sh

--- config/port-forward-delete.sh
+++ config/port-forward-delete.sh
@@ -3,8 +3,8 @@ local_port=$2
 remort_addr=$3
 remort_port=$4
 
-iptables -t nat -A PREROUTING -p tcp --dport $local_port -j DNAT --to-destination $remort_addr:$remort_port
-iptables -t nat -A POSTROUTING -p tcp -d $remort_addr --dport $remort_port -j SNAT --to-source $local_addr
+iptables -t nat -D PREROUTING -p tcp --dport $local_port -j DNAT --to-destination $remort_addr:$remort_port
+iptables -t nat -D POSTROUTING -p tcp -d $remort_addr --dport $remort_port -j SNAT --to-source $local_addr
 
-iptables -t nat -A PREROUTING -p udp --dport $local_port -j DNAT --to-destination $remort_addr:$remort_port
-iptables -t nat -A POSTROUTING -p udp -d $remort_addr --dport $remort_port -j SNAT --to-source $local_addr
+iptables -t nat -D PREROUTING -p udp --dport $local_port -j DNAT --to-destination $remort_addr:$remort_port
+iptables -t nat -D POSTROUTING -p udp -d $remort_addr --dport $remort_port -j SNAT --to-source $local_addr
